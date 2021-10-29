11287deac8582822966f38c126c1fc5c642ac284
Mayfly277 <mayfly277@gmail.com>
2021-10-29 12:35:16 +0200

    change in push method

--- pushtopip.sh
+++ pushtopip.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python3 setup.py bdist_wheel
+rm -rf dist/*
+python3 setup.py check
+python3 setup.py sdist
+python3 setup.py bdist_wheel --universal
 python3 -m twine upload --repository pypi dist/*
