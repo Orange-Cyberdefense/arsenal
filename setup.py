b3e61a37c3037a5284204cb282c3b659c89455ce
Mayfly277 <mayfly277@gmail.com>
2022-02-19 17:47:51 +0100

    version 1.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
 setup(
     name='arsenal-cli',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
