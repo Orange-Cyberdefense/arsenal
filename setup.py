cf2835215b82ff8657ddb8cfdd4c5422b457e697
Mayfly277 <mayfly277@gmail.com>
2024-03-20 08:57:13 +0100

    change version to 1.2.0 and pushed to pip

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
 setup(
     name='arsenal-cli',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
